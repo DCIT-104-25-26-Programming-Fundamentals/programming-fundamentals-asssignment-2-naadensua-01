339f983273cfe9a567f7e7b949d750fa61d4d375
naadensua-01 <narteynaadensuaotoo@gmail.com>
2026-07-30 19:17:35 +0000

    final work

--- assignment_02_student_grade_system.cpp
+++ assignment_02_student_grade_system.cpp
@@ -46,3 +46,33 @@
 #include <iostream>
 using namespace std;
 
+char getGrade(float score){
+  if (score < 0 || score > 100) {
+    return '\0';
+  }
+  if (score >= 80){
+    return 'A';
+  } else if (score >= 70){
+    return 'B';
+  } else if ( score >= 60){
+    return 'C';
+  } else if ( score >= 50){
+    return 'D';
+  }else return 'F';
+  
+}
+
+int main() {
+  int score;
+  cout << "Enter student score: ";
+  cin >> score ;
+
+  char grade= getGrade(score);
+  if (grade == '\0'){
+    cout << "Error: invalid score ";
+  }
+   else {cout << "Grade: " << grade << endl;
+  }
+  return 0;
+}
+
