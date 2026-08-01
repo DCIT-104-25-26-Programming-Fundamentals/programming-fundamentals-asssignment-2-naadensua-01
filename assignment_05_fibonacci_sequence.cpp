32eb38f4016fe7d8622a98eeb90fdbf542b012c5
naadensua-01 <narteynaadensuaotoo@gmail.com>
2026-08-01 23:21:53 +0000

    my complete work

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -50,4 +50,61 @@
 
 #include <iostream>
 using namespace std;
+void printFibonacciTerms() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: number of terms must be a positive integer." << endl;
+        return;
+    }
+
+    cout << "Fibonacci sequence: ";
+    long long a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        long long next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+void checkFibonacciNumber() {
+    long long num;
+    cout << "Enter a number to check: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << num << " is NOT a Fibonacci number." << endl;
+        return;
+    }
+
+    long long a = 0, b = 1;
+    bool found = false;
+    if (num == 0) {
+        found = true;
+    } else {
+        while (a <= num) {
+            if (a == num) {
+                found = true;
+                break;
+            }
+            long long next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    if (found) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+}
+int main() {
+    printFibonacciTerms();
+    checkFibonacciNumber();
+    return 0;
+}
 
